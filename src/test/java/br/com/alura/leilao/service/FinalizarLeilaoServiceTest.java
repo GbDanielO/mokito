a5bccdf41891aafb9458ef0cfd35b88d4bd504f7
Daniel Oliveira <gbdaniel_rj@hotmail.com>
2021-05-08 22:47:03 -0300

    adicionando comentários

--- src/test/java/br/com/alura/leilao/service/FinalizarLeilaoServiceTest.java
+++ src/test/java/br/com/alura/leilao/service/FinalizarLeilaoServiceTest.java
@@ -17,88 +17,90 @@ import br.com.alura.leilao.model.Leilao;
 import br.com.alura.leilao.model.Usuario;
 
 class FinalizarLeilaoServiceTest {
-	
-	private FinalizarLeilaoService service;
-	
-	@Mock
-	private LeilaoDao leilaoDao;
-	
-	@Mock
-	private EnviadorDeEmails enviadorDeEmails;
-	
-	@BeforeEach
-	public void beforeEach() {
-		MockitoAnnotations.initMocks(this);
-		this.service = new FinalizarLeilaoService(leilaoDao, enviadorDeEmails);
-	}
 
-	@Test
-	void deveriaFinalizarUmLeilao() {
-		List<Leilao> leiloes = leiloes();
-		
-		Mockito.when(leilaoDao.buscarLeiloesExpirados())
-		.thenReturn(leiloes);
-		
-		service.finalizarLeiloesExpirados();
-		
-		Leilao leilao = leiloes.get(0);
-		Assert.assertTrue(leilao.isFechado());
-		Assert.assertEquals(new BigDecimal("900"), 
-				leilao.getLanceVencedor().getValor());
-		
-		Mockito.verify(leilaoDao).salvar(leilao);
-	}
+    private FinalizarLeilaoService service;
 
-	@Test
-	void deveriaEnviarEmailParaVencedorDoLeilao() {
-		List<Leilao> leiloes = leiloes();
-		
-		Mockito.when(leilaoDao.buscarLeiloesExpirados())
-		.thenReturn(leiloes);
-		
-		service.finalizarLeiloesExpirados();
-		
-		Leilao leilao = leiloes.get(0);
-		Lance lanceVencedor = leilao.getLanceVencedor();
-		
-		Mockito.verify(enviadorDeEmails)
-		.enviarEmailVencedorLeilao(lanceVencedor);
-	}
-	
-	@Test
-	void naoDeveriaEnviarEmailParaVencedorDoLeilaoEmCasoDeErroAoEncerrarOLeilao() {
-		List<Leilao> leiloes = leiloes();
-		
-		Mockito.when(leilaoDao.buscarLeiloesExpirados())
-		.thenReturn(leiloes);
-		
-		Mockito.when(leilaoDao.salvar(Mockito.any()))
-		.thenThrow(RuntimeException.class);
-		
-		try {
-			service.finalizarLeiloesExpirados();
-			Mockito.verifyNoInteractions(enviadorDeEmails);
-		} catch (Exception e) {}
-	}
+    // Toda propriedade com essa anotação o mockito vai mockar no inicializar
+    @Mock
+    private LeilaoDao leilaoDao;
 
-	private List<Leilao> leiloes() {
-		List<Leilao> lista = new ArrayList<>();
-		
-		Leilao leilao = new Leilao("Celular", 
-				new BigDecimal("500"), 
-				new Usuario("Fulano"));
-		
-		Lance primeiro = new Lance(new Usuario("Beltrano"), 
-				new BigDecimal("600"));
-		Lance segundo = new Lance(new Usuario("Ciclano"), 
-				new BigDecimal("900"));
-		
-		leilao.propoe(primeiro);
-		leilao.propoe(segundo);
-		
-		lista.add(leilao);
-		
-		return lista;
-	}
+    @Mock
+    private EnviadorDeEmails enviadorDeEmails;
+
+    @BeforeEach
+    public void inicializar() {
+        // inicializa os mocks
+        MockitoAnnotations.initMocks( this );
+        this.service = new FinalizarLeilaoService( leilaoDao, enviadorDeEmails );
+    }
+
+    @Test
+    void deveriaFinalizarUmLeilao() {
+        List<Leilao> leiloes = leiloes();
+
+        // Define um retorno para o método mockado
+        Mockito.when( leilaoDao.buscarLeiloesExpirados() ).thenReturn( leiloes );
+
+        service.finalizarLeiloesExpirados();
+
+        Leilao leilao = leiloes.get( 0 );
+
+        // testes
+        Assert.assertTrue( leilao.isFechado() );
+        Assert.assertEquals( new BigDecimal( "900" ), leilao.getLanceVencedor().getValor() );
+
+        // verifica se determinado método foi chamado. Nesse caso temos o parametro.
+        // Poderia passar Mockito.any() caso não tivesse o parametro.
+        Mockito.verify( leilaoDao ).salvar( leilao );
+    }
+
+    @Test
+    void deveriaEnviarEmailParaVencedorDoLeilao() {
+        List<Leilao> leiloes = leiloes();
+
+        // Define um retorno para o método mockado
+        Mockito.when( leilaoDao.buscarLeiloesExpirados() ).thenReturn( leiloes );
+
+        service.finalizarLeiloesExpirados();
+
+        Leilao leilao = leiloes.get( 0 );
+        Lance lanceVencedor = leilao.getLanceVencedor();
+
+        // verifica se determinado método foi chamado. Nesse caso temos o parametro.
+        // Poderia passar Mockito.any() caso não tivesse o parametro.
+        Mockito.verify( enviadorDeEmails ).enviarEmailVencedorLeilao( lanceVencedor );
+    }
+
+    @Test
+    void naoDeveriaEnviarEmailParaVencedorDoLeilaoEmCasoDeErroAoEncerrarOLeilao() {
+        List<Leilao> leiloes = leiloes();
+
+        Mockito.when( leilaoDao.buscarLeiloesExpirados() ).thenReturn( leiloes );
+
+        // Forçando retornar um erro ao invés de um determinado objeto.
+        Mockito.when( leilaoDao.salvar( Mockito.any() ) ).thenThrow( RuntimeException.class );
+
+        try {
+            service.finalizarLeiloesExpirados();
+            Mockito.verifyNoInteractions( enviadorDeEmails );
+        } catch ( Exception e ) {
+        }
+    }
+
+    private List<Leilao> leiloes() {
+        List<Leilao> lista = new ArrayList<>();
+
+        Leilao leilao = new Leilao( "Celular", new BigDecimal( "500" ), new Usuario( "Fulano" ) );
+
+        Lance primeiro = new Lance( new Usuario( "Beltrano" ), new BigDecimal( "600" ) );
+        Lance segundo = new Lance( new Usuario( "Ciclano" ), new BigDecimal( "900" ) );
+
+        leilao.propoe( primeiro );
+        leilao.propoe( segundo );
+
+        lista.add( leilao );
+
+        return lista;
+    }
 
 }
--- src/test/java/br/com/alura/leilao/service/GeradorDePagamentoTest.java
+++ src/test/java/br/com/alura/leilao/service/GeradorDePagamentoTest.java
@@ -22,62 +22,58 @@ import br.com.alura.leilao.model.Pagamento;
 import br.com.alura.leilao.model.Usuario;
 
 class GeradorDePagamentoTest {
-	
-	private GeradorDePagamento gerador;
-	
-	@Mock
-	private PagamentoDao pagamentoDao;
-	
-	@Captor
-	private ArgumentCaptor<Pagamento> captor;
-	
-	@Mock
-	private Clock clock;
-	
-	@BeforeEach
-	public void beforeEach() {
-		MockitoAnnotations.initMocks(this);
-		this.gerador = new GeradorDePagamento(pagamentoDao, clock);
-	}
 
-	@Test
-	void deveriaCriarPagamentoParaVencedorDoLeilao() {
-		Leilao leilao = leilao();
-		Lance vencedor = leilao.getLanceVencedor();
-		
-		LocalDate data = LocalDate.of(2020, 12, 7);
-		
-		Instant instant  = data.atStartOfDay(ZoneId.systemDefault()).toInstant();
-		
-		Mockito.when(clock.instant()).thenReturn(instant);
-		Mockito.when(clock.getZone()).thenReturn(ZoneId.systemDefault());
-		
-		gerador.gerarPagamento(vencedor);
-		
-		Mockito.verify(pagamentoDao).salvar(captor.capture());
-		
-		Pagamento pagamento = captor.getValue();
-		
-		Assert.assertEquals(LocalDate.now().plusDays(1), 
-				pagamento.getVencimento());
-		Assert.assertEquals(vencedor.getValor(), pagamento.getValor());
-		Assert.assertFalse(pagamento.getPago());
-		Assert.assertEquals(vencedor.getUsuario(), pagamento.getUsuario());
-		Assert.assertEquals(leilao, pagamento.getLeilao());
-	}
-	
-	private Leilao leilao() {
-		Leilao leilao = new Leilao("Celular", 
-				new BigDecimal("500"), 
-				new Usuario("Fulano"));
-		
-		Lance lance = new Lance(new Usuario("Ciclano"), 
-				new BigDecimal("900"));
-		
-		leilao.propoe(lance);
-		leilao.setLanceVencedor(lance);
-		
-		return leilao;
-	}
+    private GeradorDePagamento gerador;
+
+    @Mock
+    private PagamentoDao pagamentoDao;
+
+    @Captor
+    private ArgumentCaptor<Pagamento> captor;
+
+    @Mock
+    private Clock clock;
+
+    @BeforeEach
+    public void beforeEach() {
+        MockitoAnnotations.initMocks( this );
+        this.gerador = new GeradorDePagamento( pagamentoDao, clock );
+    }
+
+    @Test
+    void deveriaCriarPagamentoParaVencedorDoLeilao() {
+        Leilao leilao = leilao();
+        Lance vencedor = leilao.getLanceVencedor();
+
+        LocalDate data = LocalDate.of( 2020, 12, 7 );
+
+        Instant instant = data.atStartOfDay( ZoneId.systemDefault() ).toInstant();
+
+        Mockito.when( clock.instant() ).thenReturn( instant );
+        Mockito.when( clock.getZone() ).thenReturn( ZoneId.systemDefault() );
+
+        gerador.gerarPagamento( vencedor );
+
+        Mockito.verify( pagamentoDao ).salvar( captor.capture() );
+
+        Pagamento pagamento = captor.getValue();
+
+        Assert.assertEquals( LocalDate.now().plusDays( 1 ), pagamento.getVencimento() );
+        Assert.assertEquals( vencedor.getValor(), pagamento.getValor() );
+        Assert.assertFalse( pagamento.getPago() );
+        Assert.assertEquals( vencedor.getUsuario(), pagamento.getUsuario() );
+        Assert.assertEquals( leilao, pagamento.getLeilao() );
+    }
+
+    private Leilao leilao() {
+        Leilao leilao = new Leilao( "Celular", new BigDecimal( "500" ), new Usuario( "Fulano" ) );
+
+        Lance lance = new Lance( new Usuario( "Ciclano" ), new BigDecimal( "900" ) );
+
+        leilao.propoe( lance );
+        leilao.setLanceVencedor( lance );
+
+        return leilao;
+    }
 
 }
